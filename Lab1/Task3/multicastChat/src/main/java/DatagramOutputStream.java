19519bfb943f8e7a2e198d8dc68f27b985f68b56
mmajews <majewskimarcin@outlook.com>
2016-03-14 14:40:24 +0100

    Quick Fixes

--- Lab1/Task3/multicastChat/src/main/java/DatagramOutputStream.java
+++ Lab1/Task3/multicastChat/src/main/java/DatagramOutputStream.java
@@ -5,14 +5,14 @@ public class DatagramOutputStream extends ByteArrayOutputStream {
     DatagramPacket packet;
 
     public DatagramOutputStream(DatagramSocket socket, InetAddress address, int port, int initialSize) {
-        super (initialSize);
+        super(initialSize);
         this.socket = socket;
-        packet = new DatagramPacket (buf, 0, address, port);
+        packet = new DatagramPacket(buf, 0, address, port);
     }
 
     public synchronized void flush () throws IOException {
-        packet.setData (buf, 0, count);
-        socket.send (packet);
+        packet.setData(buf, 0, count);
+        socket.send(packet);
 
         //Resetting output stream to count = 0
         reset();
