d450d3314fe3b43531fcc753d1e0ac850df5e354
mmajews <majewskimarcin@outlook.com>
2016-03-14 14:40:54 +0100

    Deleting unnecessary defaults

--- Lab1/Task3/multicastChat/src/main/java/Main.java
+++ Lab1/Task3/multicastChat/src/main/java/Main.java
@@ -2,12 +2,6 @@ import java.io.IOException;
 import java.net.InetAddress;
 
 public class Main {
-
-
-    static final String DEFAULT_GROUP = "239.1.2.3";
-    static final int DEFAULT_PORT = 1234;
-    static final int DEFAULT_TTL = 1;
-
     public static void main (String[] args) throws IOException {
         if ((args.length > 3) || ((args.length <= 0))) {
             System.out.println("Wrong number of arguments!");
