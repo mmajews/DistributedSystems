e2b634e1f401dd76d82553994e7855a3c4b9cfcc
mmajews <majewskimarcin@outlook.com>
2016-04-09 21:47:59 +0200

    * Fixing problems with poms

---
+++ Lab3/taskSolutionReceiver/src/main/java/solutionReceiver/SolutionReceiverApplication.java
@@ -0,0 +1,12 @@
+package solutionReceiver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+
+@SpringBootApplication
+@EnableJms
+public class SolutionReceiverApplication {
+
+    public static void main(String[] args) {
+    }
+}
