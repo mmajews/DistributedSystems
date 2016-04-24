587fdbb0f77c98f599ed4ac4c645eefdb2f5194a
mmajews <majewskimarcin@outlook.com>
2016-04-24 15:01:30 +0200

    * Task1Locator implemented * Adding guava to program * Adding sample state

--- Lab4/Zad1/src/main/java/server/Server.java
+++ Lab4/Zad1/src/main/java/server/Server.java
@@ -1,16 +1,22 @@
 package server;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import impl.Echo;
 import org.apache.log4j.Logger;
 
 public class Server {
 
     private static Logger logger = Logger.getLogger(Server.class);
 
-    private void serverStart(){
+    private void serverStart() {
         logger.debug("serverStart() method invoked");
         int status = 0;
         Ice.Communicator communicator = null;
+
+        Echo echo = new Echo();
+        Gson gson = new GsonBuilder().create();
+        System.out.println(gson.toJson(echo));
 
     }
 
--- Lab4/Zad1/src/main/java/server/Task1Locator.java
+++ Lab4/Zad1/src/main/java/server/Task1Locator.java
@@ -2,25 +2,56 @@ package server;
 
 import Ice.*;
 import Ice.Object;
+import com.google.common.base.Preconditions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import impl.Echo;
 import org.apache.log4j.Logger;
 
-public class Task1Locator extends Locator{
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.Exception;
+
+public class Task1Locator extends Locator {
     private static final Logger logger = Logger.getLogger(Task1Locator.class);
     private final ObjectAdapter objectAdapter;
     private static final Gson gson = new GsonBuilder().create();
-
+    private static final String fileName = "src/main/resources/savedState.json";
 
     public Task1Locator(String id, ObjectAdapter objectAdapter) {
         super(id);
         this.objectAdapter = objectAdapter;
+        logger.info(String.format("Task1Locator with id: %s started", id));
     }
 
 
     @Override
     public Object locate(Current current, LocalObjectHolder localObjectHolder) throws UserException {
-        return null;
+        Object servant = objectAdapter.find(current.id);
+        if (servant == null) {
+            Echo echo = null;
+            File file = new File(fileName);
+            if (file.exists()) {
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+                    echo = gson.fromJson(bufferedReader, Echo.class);
+                } catch (FileNotFoundException e) {
+                    logger.error("Error while reading from file.. Exiting", e);
+                    System.exit(1);
+                }
+            } else {
+                logger.warn(String.format("File with path %s does not exist. New will be created", file.getAbsolutePath()));
+                echo = new Echo();
+
+            }
+            Preconditions.checkNotNull(echo, "Echo must not be null!");
+            objectAdapter.add(echo, current.id);
+            return echo;
+        } else {
+            return servant;
+        }
     }
 
 }
