b8bd26774a304060fe7d30d2cf7aa94ea55b7661
mmajews <majewskimarcin@outlook.com>
2016-04-24 14:13:38 +0200

    * Configuring log4j properties

--- Lab4/Zad1/src/main/java/server/Server.java
+++ Lab4/Zad1/src/main/java/server/Server.java
@@ -6,7 +6,16 @@ public class Server {
 
     private static Logger logger = Logger.getLogger(Server.class);
 
+    private void serverStart(){
+        logger.info("serverStart() method invoked");
+        int status = 0;
+        Ice.Communicator communicator = null;
+    }
+
+
     public static void main(String[] args) {
         logger.info("Starting server...");
+        Server server = new Server();
+        server.serverStart();
     }
 }
